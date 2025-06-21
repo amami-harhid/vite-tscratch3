5f3be1ab62d374accd8dd62fe59c0a3fa1428303
amami.thidamall.0001 <amami.thidamall.0001@gmail.com>
2025-06-21 02:20:34 +0000

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
 'use strict'
+import erasableSyntaxOnly from "eslint-plugin-erasable-syntax-only";
+import tseslint from "typescript-eslint";
 import {eslint_S3_config} from "@tscratch3/tscratch3likejs/eslint-plugin/index.js";
-export default eslint_S3_config;
+export default eslint_S3_config
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,39 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import glob from 'glob'
+import topLevelAwait from 'vite-plugin-top-level-await';
+
 const root = resolve(__dirname, 'src')
+const entries = glob.sync('./src/**/index.html');
+const targetDir = []
+for(const entry of entries) {
+    const directory = entry.replace('./src/', '').replace(/\/index\.html$/,'')
+    targetDir.push(directory)
+}
+const rollupOpsionsInput = {}
+for(const target of targetDir){
+    rollupOpsionsInput[target] = resolve(root, target, 'index.html')
+}
+
 const outDir = resolve(__dirname, 'dist');
 export default defineConfig({
+    plugins: [topLevelAwait()],
     build: {
+        target: "esnext",
         outDir,
         rollupOptions: {
-            input: {
-                test02: resolve(root, 'test02', 'index.html'),
-            },
+            input: rollupOpsionsInput,
         },
-        target: "es2022"
     },
     esbuild: {
-        target: "es2022"
+        supported: {
+            'top-level-await': true
+        },
+        target: "esnext"
     },
     optimizeDeps:{
         esbuildOptions: {
-            target: "es2022",
+            target: "esnext",
         }
     },
     root,
